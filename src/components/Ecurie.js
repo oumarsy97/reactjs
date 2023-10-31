63f2c1579b69e5898e9ecf05e3c722da0f9b4555
Oumar Sy <78902232+oumarsy97@users.noreply.github.com>
2023-10-31 12:17:00 +0000

    ok

--- src/components/Ecurie.js
+++ src/components/Ecurie.js
@@ -7,20 +7,12 @@ export default function Ecurie(props) {
     {id:2, pseudo: "Siteu"},
     {id:3, pseudo: "Ama Baldé"}
   ]);
-if(!lutteurs.length) {
-  return(
-    <div>
-      <h1>La liste des lutteurs de l'écurie {props.nom}</h1>
-      <p>Aucun lutteur</p>
-    </div>
-  )
-}
+//     
   return (
     <div>
       <h1>La liste des lutteurs de l'écurie {props.nom}</h1>
       <ul>
-      
-        {lutteurs.map(lutteur => (
+      {(!lutteurs.length)?<p>Aucun lutteur</p>:lutteurs.map(lutteur => (
           <Lutteur pseudo={lutteur.pseudo} key={lutteur.id}/>
         ))}
       </ul>
